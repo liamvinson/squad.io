be8909b59d06c3d483131d716f794f9807b09f12
liamvinson <liamvinson1@gmail.com>
2024-08-15 20:53:43 +0100

    add main page and styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="main">
+    <h1>Squad.</h1>
+
+    <p>Squad is an app to generate randomised teams from a list of players.</p>
+
+    <a href="/privacy">Privacy Policy</a>
+</div>
+
+<style>
+    :global(html) {
+        font-family: "Helvetica Neue", sans-serif;
+        background: #020202;
+        color: white;
+    }
+
+    h1 {
+        color: #1097b5;
+    }
+
+    .main {
+        width: 600px;
+        margin: 100px auto;
+    }
+
+    a {
+        color: #b7b7b7
+    }
+</style>
