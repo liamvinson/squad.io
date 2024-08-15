42c16f4b42ab75e57b319ef50ffe51592db88711
liamvinson <liamvinson1@gmail.com>
2024-08-15 17:30:38 +0100

    add privacy policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,53 @@
+<p>
+Squad. Privacy Policy
+</p>
+
+<p>
+Your privacy is important to us at Squad. This policy outlines how we collect, use, and protect your personal information within the Squad. app.
+</p>
+
+<p>
+Information We Collect
+</p>
+
+<p>
+We only collect the names you enter within the app. This information is essential for the app's functionality and to provide you with a personalized experience. We do not collect any other personal information about you.
+</p>
+
+<p>
+How We Use Your Information
+</p>
+
+<p>
+The names you enter are used solely within the app to provide you with a personalized user experience. We do not share or sell your name with any third parties.
+</p>
+
+<p>
+Data Storage
+</p>
+
+<p>
+We use SwiftData, a secure data storage framework from Apple, to store the names you enter within the app. Your data is encrypted and protected according to Apple's strict privacy standards.
+</p>
+
+<p>
+Your Rights
+</p>
+
+<p>
+You have the right to review, update, or delete your name within the app at any time. You can do so by accessing the app's settings or contacting us directly.
+</p>
+
+<p>
+Security
+</p>
+
+<p>
+We take the security of your data seriously. We have implemented robust security measures to protect your name from unauthorized access or disclosure.
+</p>
+
+<p>
+Changes to This Policy
+</p>
+
+We reserve the right to update this privacy policy at any time. We will notify you of any changes through the app or by other means.
